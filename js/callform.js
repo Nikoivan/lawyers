8c35f5879a2f53e7bf9a0e87c1e66f6b291a0379
Niko Ivan <nixonivan@rambler.ru>
2024-10-03 21:28:00 +0300

    fix

--- js/callform.js
+++ js/callform.js
@@ -8,25 +8,33 @@ class CallForm {
 	}
 
 	init() {
+		this.body = document.body;
 		this.callForm = document.querySelector('.callform');
+		this.callFormWrap = document.querySelector('.callform__wrapper');
 		this.openBtn = document.querySelector('.authors__btn');
 		this.closeBtn = document.querySelector('.callform__btn_type_close');
+		this.successCloseBtn = document.querySelector('.callform__success-btn_type_close');
 		this.activeBtn = document.querySelector('.callform__btn_type_action');
+		this.successWrap = document.querySelector('.callform__wrapper_type_success');
 
 		this.handleOpenBtnClick = this.handleOpenBtnClick.bind(this);
 		this.handleCloseBtnClick = this.handleCloseBtnClick.bind(this);
 		this.handleActiveBtnClick = this.handleActiveBtnClick.bind(this);
+		this.handleCloseSuccessWrap = this.handleCloseSuccessWrap.bind(this);
+		this.handleSubmitClickBtn = this.handleSubmitClickBtn.bind(this);
 
 		this.openBtn.addEventListener('click', this.handleOpenBtnClick);
 		this.closeBtn.addEventListener('click', this.handleCloseBtnClick);
-		this.activeBtn.addEventListener('click', this.handleActiveBtnClick);
+		this.activeBtn.addEventListener('click', this.handleSubmitClickBtn);
+		this.successCloseBtn.addEventListener('click', this.handleCloseSuccessWrap);
 	}
 
 	handleOpenBtnClick(e) {
 		e.preventDefault();
 
 		this.callForm.classList.add('active');
-		body.classList.add('noscroll');
+		this.callFormWrap.classList.add('active');
+		this.body.classList.add('noscroll');
 	}
 
 	handleCloseBtnClick(e) {
@@ -34,16 +42,46 @@ class CallForm {
 			e.preventDefault();
 		}
 
-		this.callForm.classList.remove('active');
-		body.classList.remove('noscroll');
+		setTimeout(() => {
+			this.callForm.classList.remove('active');
+			this.body.classList.remove('noscroll');
+		}, 500);
+		this.callFormWrap.classList.remove('active');
 	}
 
 	handleActiveBtnClick(e) {
 		e.preventDefault();
 
+		this.callFormWrap.classList.remove('active');
+
 		setTimeout(() => {
-			this.handleCloseBtnClick.call(this);
-		}, 500);
+			this.openSuccessWrap.call(this);
+		}, 600);
+	}
+
+	openSuccessWrap() {
+		this.successWrap.classList.add('active');
+
+		setTimeout(() => {
+			this.handleCloseSuccessWrap.call(this);
+		}, 1500);
+	}
+
+	handleCloseSuccessWrap(e) {
+		if (e) {
+			e.preventDefault();
+		}
+
+		this.successWrap.classList.remove('active');
+		this.callForm.classList.remove('active');
+		this.body.classList.remove('noscroll');
+	}
+
+	handleSubmitClickBtn(e) {
+		e.preventDefault();
+
+		this.callFormWrap.classList.remove('active');
+		this.successWrap.classList.add('active');
 	}
 }
 
