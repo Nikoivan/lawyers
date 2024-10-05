093519d0747bf7cbc4afda8856ca9e0dc48c0c67
Niko Ivan <nixonivan@rambler.ru>
2024-10-05 21:33:05 +0300

    slider in process

---
+++ js/slider.js
@@ -0,0 +1,54 @@
+class Slider {
+	constructor() {
+		document.addEventListener('DOMContentLoaded', () => {
+			this.slider = document.querySelector('.slider__wrap');
+			this.sliderList = document.querySelector('.slider__list');
+			this.items = document.querySelectorAll('.slider__item');
+			this.count = 0;
+			this.width;
+
+			this.init = this.init.bind(this);
+			this.rollSlider = this.rollSlider.bind(this);
+			this.nextSlide = this.nextSlide.bind(this);
+
+			window.addEventListener('resize', this.init);
+
+			this.init();
+		});
+	}
+
+	init() {
+		this.width = this.slider.offsetWidth / 2 - 21;
+		this.sliderList.style.width = this.width * this.items.length + 'px';
+
+		this.items.forEach((item) => {
+			item.style.width = this.width + 'px ';
+		});
+
+		this.rollSlider();
+
+		setTimeout(() => {
+			this.nextSlide();
+		}, 3000);
+	}
+
+	nextSlide() {
+		this.count++;
+
+		if (this.count >= this.items.length - 1) {
+			this.count = 0;
+		}
+
+		this.rollSlider();
+
+		setTimeout(() => {
+			this.nextSlide();
+		}, 3000);
+	}
+
+	rollSlider() {
+		this.sliderList.style.transform = 'translate(-' + this.count * this.width + 'px)';
+	}
+}
+
+const slider = new Slider();
